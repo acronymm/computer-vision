0cd59404cd3fb73dad2571121494a16e2c21b9ef
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-04-27 14:23:24 +0900

    histogram stretching & equalization

--- ch06.py
+++ ch06.py
@@ -59,7 +59,71 @@ def posterize():
     cv2.destroyAllWindows()
 
 
+def histogramStretching():
+    filename = 'images/akrom.jpeg'
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('image', img)
+
+    Hist = np.zeros(256)
+    ysize = img.shape[0]
+    xsize = img.shape[1]
+    for y in range(ysize):
+        for x in range(xsize):
+            Hist[img.item(y,x)] += 1
+
+    low = 0
+    high = 255
+    for i in range(256):
+        if Hist[i] != 0:
+            low = i
+            break
+
+    for i in range(255, -1, -1):
+        if Hist[i] != 0:
+            high = i
+            break
+
+    for y in range(ysize):
+        for x in range(xsize):
+            value = round((img.item(y,x) - low)/(high - low) * 255)
+            img.itemset((y,x), value)
+
+    cv2.imshow('result', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def histogram_equialization():
+    filename="images/akrom.jpeg"
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('image', img)
+
+    Hist = np.zeros((256))
+    ysize = img.shape[0]
+    xsize = img.shape[1]
+    for y in range(ysize):
+        for x in range(xsize):
+            Hist[img.item(x,y)] += 1
+
+    normHist = np.empty((256))
+    sum = 0.0
+    factor = 255.0 / (ysize * xsize)
+    for i in range(256):
+        sum += Hist[i]
+        normHist[i] = round(sum * factor)
+
+    for y in range(ysize):
+        for x in range(xsize):
+            img.itemset((y,x), normHist[img.item(y,x)])
+
+    cv2.imshow('result', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     # check_pixels()
-    posterize()
+    # posterize()
+    histogramStretching()
+    # histogram_equialization()
 
