a43f01dd5db5516b064823e95a5bdaff5e7a7ac2
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-05-25 16:30:28 +0900

    hw #2: region processing

--- ch07.py
+++ ch07.py
@@ -2,16 +2,70 @@ import cv2
 import numpy as np
 
 
+def filter2D():
+    filename = 'images/fruits.jpg'
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    # high-pass filter
+    kernel = np.array([[-1, -1, -1],
+                       [-1, 9, -1],
+                       [-1, -1, -1]])
+    result = cv2.filter2D(img, -1, kernel)
+    cv2.imshow('image', img)
+    cv2.imshow('result', result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def canny():
+    filename = 'images/lena.jpg'
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    result = cv2.Canny(img, 50, 150)
+    cv2.imshow('image', img)
+    cv2.imshow('Canny', result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def erodeNdilate():
+    filename = 'images/char.jpg'
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    height, width = img.shape[0:2]
+    kernel = np.ones((3, 3), np.uint8)
+    result1 = cv2.erode(img, kernel, iterations=1)
+    result2 = cv2.dilate(img, kernel, iterations=1)
+    cv2.imshow('image', img)
+    cv2.imshow('erosion', result1)
+    cv2.imshow('dilation', result2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def morphologyEx():
+    filename = 'images/char.jpg'
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    height, width = img.shape[0:2]
+    kernel = np.ones((3, 3), np.uint8)
+    result1 = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    result2 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    result3 = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+    cv2.imshow('image', img)
+    cv2.imshow('opening', result1)
+    cv2.imshow('closing', result2)
+    cv2.imshow('gradient', result3)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def applyFilter(filename):
     image = cv2.imread(filename, cv2.IMREAD_COLOR)
 
     # high-pass filter
     highPass = np.array([
-        [-1,-1,-1],
-        [-1, 9,-1],
-        [-1,-1,-1],
+        [-1, -1, -1],
+        [-1, 9, -1],
+        [-1, -1, -1],
     ])
-    
+
     # low-pass Gaussian filter
     lowPassGaussian = np.array([
         [0, 0, 1, 0, 0],
@@ -25,7 +79,7 @@ def applyFilter(filename):
     lowPassGaussian = lowPassGaussian/lowPassGaussian.sum()
 
     # low-pass ones filter
-    lowPassOnes = np.full((5,5), 1)
+    lowPassOnes = np.full((5, 5), 1)
 
     lowPassOnes = lowPassGaussian/lowPassGaussian.sum()
 
@@ -42,5 +96,9 @@ def applyFilter(filename):
 
 
 if __name__ == '__main__':
-    filename = 'images/lena.jpg'
-    applyFilter(filename)
+    erodeNdilate()
+    # morphologyEx()
+    # canny()
+    # filter2D()
+    # filename = 'images/lena.jpg'
+    # applyFilter(filename)
